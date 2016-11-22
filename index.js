506270d120bb0c40ecb211a7d6ea9076e8417c4f
Thomas Watson Steen <w@tson.dk>
2016-11-22 13:52:45 +0100

    fix cwd bug

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var pump = require('pump')
 var multi = require('multistream')
 
 var streams = [
-  fs.createReadStream(path.join('templates', 'head.html')),
+  fs.createReadStream(path.join(__dirname, 'templates', 'head.html')),
   process.stdin,
-  fs.createReadStream(path.join('templates', 'tail.html'))
+  fs.createReadStream(path.join(__dirname, 'templates', 'tail.html'))
 ]
 
 pump(multi(streams), process.stdout)
